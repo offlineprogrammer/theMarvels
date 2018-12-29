9030d5ec749e1d4754e151c8471cd9a8f0f2852c
OfflineProgrammer <mmalaka@gmail.com>
2018-12-28 22:16:22 -0800

    standard_medium

--- src/app/interfaces/character.ts
+++ src/app/interfaces/character.ts
@@ -2,10 +2,12 @@ export class character {
     id:string;
     name:string;
     thumbnail:string;
+    card:string;
 
     constructor(item: any) {
         this.id = item.id;
         this.name = item.name;
-        this.thumbnail =  item.thumbnail.path + '/portrait_medium.'+item.thumbnail.extension;
+        this.thumbnail =  item.thumbnail.path + '/standard_medium.'+item.thumbnail.extension;
+        this.card =  item.thumbnail.path + '/standard_xlarge.'+item.thumbnail.extension;
     }
 }
