8f9a3fd023387c65f8038cf3adbbed7f4f16f6f3
OfflineProgrammer <mmalaka@gmail.com>
2019-01-01 22:11:52 -0800

    Splash

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', loadChildren: './home/home.module#HomePageModule' },
   { path: 'details/:id', loadChildren: './detail/detail.module#DetailPageModule' },
+  { path: 'splash', loadChildren: './splash/splash.module#SplashPageModule' },
 ];
 
 @NgModule({
